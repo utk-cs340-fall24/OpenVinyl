026117f137acd239b21e94fb6510f24e23b9638e
Dylan Toth <dylantoth@dylansmac110.nomads.utk.edu>
2024-09-17 12:18:43 -0400

    Hello world dtoth2

---
+++ helloplus/dtoth2/README.md
@@ -0,0 +1,10 @@
+# Hello world program
+This is a hello world sveltekit program that generates a random chuck norris joke as well as a dog.
+
+# Running
+1. cd into the hello-world-svelte directory
+2. npm install
+3. npm run dev -- --open
+
+# Usage
+Sveltekit will be the web framework of choice for our project.
---
+++ helloplus/dtoth2/hello-world-svelte/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+    /** @type {import('./$types').ActionData} */
+	export let action_data;
+
+</script>
+<h1> Hello World </h1>
+<form method="POST" action="?/generateJoke">
+<button>
+	Generate new random Chuck Norris joke and a dog
+</button>
+</form>
+
+<h1>{action_data?.chuck == null ? (data.chuck.value) : action_data.chuck.value}</h1>
+<img alt="Dog" src={action_data?.dog == null ? data.dog.message : action_data?.dog.message} />
