953e0bc58a49c88223901bef7706d7138934c445
wessoms <sessoms.will@gmail.com>
2024-09-17 18:06:38 -0400

    wsessoms helloplus

---
+++ helloplus/wsessoms/README.md
@@ -0,0 +1,14 @@
+# Description
+
+This is a basic Svelte program that puts a random cover art from the band "julie" on the screen. The art can be randomly changed with the button at the center of the page.
+
+# To Run
+
+ - clone the repository
+ - cd into the ```album-covers``` directory
+ - with npm installed, run ```npm install```
+ - run ```npm run dev``` and open the localhost link
+
+# Use
+
+We plan to use Svelte as the web framework for our project. This program uses the cover art urls given by the Spoitfy API, so we know those will be easy to display in the actual project.
---
+++ helloplus/wsessoms/album-covers/src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+
+	let images = [
+		"https://i.scdn.co/image/ab67616d0000b2738c8ba2255c9813ced88ac90c",
+		"https://i.scdn.co/image/ab67616d0000b2737532f8f3e58551b996e3b843",
+		"https://i.scdn.co/image/ab67616d0000b2732366e6480396347943c45cef",
+		"https://i.scdn.co/image/ab67616d0000b2731465ed739867d850aeb6aa71",
+		"https://i.scdn.co/image/ab67616d0000b273420ea174ee8015c3f6dc683b",
+		"https://i.scdn.co/image/ab67616d0000b2730a4a4412593bd5bfc5e9844b",
+		"https://i.scdn.co/image/ab67616d0000b27345a30e97e37a879227f27679",
+		"https://i.scdn.co/image/ab67616d0000b2736047db4c19dc348070cb1a64"
+	]
+
+	let currentImage = images[0];
+
+	function getRandomImage() {
+    	const randomIndex = Math.floor(Math.random() * images.length); // Pick a random image index
+		currentImage = images[randomIndex];
+  	}
+</script>
+
+<main class ="container">
+	<img src={currentImage} alt="Random Cover Art"/>
+	<button on:click={getRandomImage}>Random julie cover art</button>
+</main>
+
+<style>
+
+	.container {
+    	display: flex;
+    	flex-direction: column;
+    	align-items: center;
+		margin: 10px;
+  	}
+
+	button {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    color: white;
+	font-weight: bold;
+    border: none;
+    border-radius: 5px;
+	margin: 20px;
+	background: linear-gradient(to top, #360000, #9b0000);
+  }
+
+	button:hover {
+		background: linear-gradient(to top, #6e0000, #d30000);
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
