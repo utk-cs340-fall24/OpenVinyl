c961af3d2f01c92b96c71175767672cb577be348
Weston49 <55215866+Weston49@users.noreply.github.com>
2024-09-19 16:26:17 -0400

    Update supabaseClient.js

--- OpenVinyl/src/lib/supabaseClient.js
+++ OpenVinyl/src/lib/supabaseClient.js
@@ -1,8 +1,3 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://mgyftmhpdafhuuvvgebv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1neWZ0bWhwZGFmaHV1dnZnZWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1MjY0NzAsImV4cCI6MjA0MjEwMjQ3MH0.n_pNbRHRpSb27FFwxf-ijtM0p1svXX0FOHcC_2smJEk')
-=======
-
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
