72282cdeca0de338b4afabeba4e18d2fac446fbe
Dylan Toth <dylant1@yahoo.com>
2024-09-22 00:50:24 -0400

    Update svelte.config.js to test netlify functions

--- OpenVinyl/svelte.config.js
+++ OpenVinyl/svelte.config.js
@@ -19,7 +19,7 @@ export default {
 		adapter: adapter({
 			// if true, will create a Netlify Edge Function rather
 			// than using standard Node-based functions
-			edge: false,
+			edge: true,
 
 			// if true, will split your app into multiple functions
 			// instead of creating a single one for the entire app.
