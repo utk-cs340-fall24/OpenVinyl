5d92bee6b9bf81bc72fa2f6c202b43985e4b77a3
Dylan Toth <dylant1@yahoo.com>
2024-09-26 11:49:02 -0400

    Add test google sign on

---
+++ OpenVinyl/src/routes/test/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { supabase } from '$lib/supabaseClient';
+  
+    const signInWithGoogle = async () => {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+      });
+      if (error) {
+        console.error('Error during sign in:', error.message);
+      }
+    };
+  </script>
+  
+  <button on:click={signInWithGoogle}>Sign in with Google</button>
+  
