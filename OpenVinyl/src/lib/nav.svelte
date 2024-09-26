53b869dcff18379ed89173b89378c809d436a2c9
grantarg <grantrjohnson@hotmail.com>
2024-09-26 12:43:17 -0400

    Login styling and routing changes

--- OpenVinyl/src/lib/nav.svelte
+++ OpenVinyl/src/lib/nav.svelte
@@ -7,7 +7,7 @@
     </div>
     <div class="right-nav">
       <p>username</p>
-      <a href="/login">Login</a>
+      <a href="/auth/signin">Login</a>
     </div>
   </nav>
 </div>
--- OpenVinyl/src/routes/auth/signin/+page.svelte
+++ OpenVinyl/src/routes/auth/signin/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import { supabase } from '$lib/supabaseClient.js';
+  import '@fortawesome/fontawesome-free/css/all.css';
+  import '@fortawesome/fontawesome-free/js/all.js';
 
   let email;
   let pass;
@@ -9,16 +11,6 @@
         window.location.href = '/'; // redirect to account page
     }
   });
-
-  async function signUp() {
-    console.log("email: ", email,"pass: ", pass);
-    const { data, error } = await supabase.auth.signUp({
-        email: email,
-        password: pass,
-    })
-    console.log("data: ", data);
-    console.log("error: ", error);
-  }
 
   async function signIn() {
     const { data, error } = await supabase.auth.signInWithPassword({
@@ -39,10 +31,21 @@
   
     <input type="email" name="" id="email" placeholder="email" bind:value={email}>
     <input type="password" name="" id="pass" placeholder="password" bind:value={pass}>
-    <button class="inputbox" on:click={signIn}>Sign In</button>
-    <a href="\login\createaccount">
-      <button class="inputbox">Create Account</button>
-    </a>
+    <div class="emailbuttons">
+      <button class="inputbox" on:click={signIn}>Sign In</button>
+    </div>
+    <hr class="hr-text" data-content="Or continue with">
+    <div class="oauthbuttons">
+      <button class="google">
+        <i class="fa-brands fa-google"></i> Google
+      </button>
+      <button class="spotify">
+        <i class="fa-brands fa-spotify"></i> Spotify
+      </button>
+      <a href="\auth\signup">
+        <button class="inputbox"><i class="fa-solid fa-envelope"></i> Email</button>
+      </a>
+    </div>
   </div>
 </div>
 
@@ -62,7 +65,7 @@
     align-items: center;
   }
 
-  .container div {
+  .container .login-window {
     height: 50vh;
     max-width: 30vw;
     background-color: #fff;
@@ -91,6 +94,57 @@
     border: 1px solid #ccc;
     border-radius: 4px;
     box-sizing: border-box;
+  }
+
+  .login-window .emailbuttons button {
+    width: 100%;
+    height: 2.5em;
+    font-size: 18px;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  .fa-google {
+    color: White;
+  }
+
+  .login-window .oauthbuttons button {
+    width: 32%;
+    height: 3em;
+  }
+
+  .hr-text {
+    line-height: 1em;
+    position: relative;
+    outline: 0;
+    border: 0;
+    color: black;
+    text-align: center;
+    height: 1.5em;
+    opacity: .5;
+  }
+  
+  .hr-text::before {
+    content: '';
+    background: linear-gradient(to right, transparent, #818078, transparent);
+    position: absolute;
+    left: 0;
+    top: 50%;
+    width: 100%;
+    height: 1px;
+  }
+  
+  .hr-text::after {
+    content: attr(data-content);
+    position: relative;
+    display: inline-block;
+    color: black;
+    padding: 0 .5em;
+    line-height: 1.5em;
+    color: #818078;
+    background-color: #fcfcfa;
   }
 </style>
 
