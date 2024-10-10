21b4bc7eed1a2634fd0054026d0f171232d46d15
Grant Johnson <grantrjohnson@hotmail.com>
2024-10-10 10:04:05 -0400

    Add username taken alert

--- src/routes/(app)/account/+page.svelte
+++ src/routes/(app)/account/+page.svelte
@@ -26,9 +26,12 @@
       return;
     }
 
-    updateUsername(user.data.user.id, username);
-    // updateFirstName(user.data.user.id, fname);
-    // updateLastName(user.data.user.id, lname);
+    const {data, error} = await updateUsername(user.data.user.id, username);
+    if (error == "duplicate key value violates unique constraint \"profiles_username_key\"") {
+      document.getElementById('username-alert').innerHTML = "<i class=\"fa-solid fa-xmark\"></i>Username taken. Please try again";
+    }
+    updateFirstName(user.data.user.id, fname);
+    updateLastName(user.data.user.id, lname);
   }
 
   async function logout() {
@@ -42,10 +45,19 @@
     <h1>Account Settings</h1>
 
     <p>Update your information below</p>
-  
-    <input type="text" name="" id="fname" placeholder="first name" bind:value={fname}>
-    <input type="text" name="" id="lname" placeholder="last name" bind:value={lname}>
-    <input type="text" name="" id="username" placeholder="username" bind:value={username}>
+    
+    <div class="input-group">
+      <input type="text" name="" id="fname" placeholder="first name" bind:value={fname}>
+      <p class="user-alert"></p>
+    </div>
+    <div class="input-group">
+      <input type="text" name="" id="lname" placeholder="last name" bind:value={lname}>
+      <p class="user-alert"></p>
+    </div>
+    <div class="input-group">
+      <input type="text" name="" id="username" placeholder="username" bind:value={username}>
+      <p class="user-alert" id="username-alert"></p>
+    </div>
     <button class="signout" on:click={logout}>Sign out</button>
     <button class="update" on:click={submitUserInfo}>Update</button>
   </div>
@@ -65,7 +77,8 @@
 
   .wrapper .account-settings {
     height: 50vh;
-    max-width: 30vw;
+    width: 25%;
+    color: #fff;
     background-color: #1d1f25;
     /* box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1); */
     border: 1px solid #26282c;
@@ -74,14 +87,23 @@
     text-align: center;
   }
 
-  .wrapper .account-settings input {
+  .wrapper .account-settings .input-group {
+    margin: 8px 0;
+  }
+
+  .wrapper .account-settings .input-group input {
     width: 100%;
     padding: 12px 20px;
-    margin: 8px 0;
+    margin: 0 0 4px 0;
     display: inline-block;
     border: 1px solid #ccc;
     border-radius: 4px;
     box-sizing: border-box;
+  }
+
+  .wrapper .account-settings .user-alert {
+    text-align: left;
+    margin: 0;
   }
 
   .wrapper .account-settings button {
